24db37c97a2931c3af0f220a0b33c9e9097fc98e
Giovanni_T <giovannitummino.15@gmail.com>
2024-06-11 20:28:59 +0200

    mise a jour

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacker Poulette</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="/assets/style.css" rel="stylesheet">
     <style>
         .error { color: red; }
     </style>
 </head>
 <body>
-<nav class="navbar bg-body-tertiary" data-bs-theme="dark">
+<nav class="navbar bg-body-tertiary" data-bs-theme="light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="/assets/hackers-poulette-logo.png" alt="Logo" width="150" height="75" class="d-inline-block align-text-top">
-      Hackers-poulette
+      <img src="/assets/hackers-poulette-logo.png" alt="Logo" width="150" height="100" class="d-inline-block align-text-top">
+      <p class="mt-2">Hackers-poulette</p>
     </a>
   </div>
 </nav>
@@ -103,18 +104,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
 }
 ?>
 
-<main class="container my-5 text-center" style = "max-width: 580px">
+<main id="container" class="container my-5 text-center shadow-lg p-3 mb-5 bg-body-tertiary rounded" style = "max-width: 580px">
 <form method="post" action="" autocomplete="off">
-    <label class="form-label" for="fname">First name:</label>
-    <input class="form-control" type="text" id="fname" name="fname" value="<?php echo isset($_POST['fname']) ? htmlspecialchars($_POST['fname']) : ''; ?>"><br><br>
+    <label class="form-label mt-n1" for="fname">First name:</label>
+    <input class="form-control border border-info mt-n1" type="text" id="fname" name="fname" value="<?php echo isset($_POST['fname']) ? htmlspecialchars($_POST['fname']) : ''; ?>"><br><br>
     <p class="error error_fname"><?php echo $fname_error; ?></p>
 
     <label class="form-label" for="lname">Last name:</label>
-    <input class="form-control" type="text" id="lname" name="lname" value="<?php echo isset($_POST['lname']) ? htmlspecialchars($_POST['lname']) : ''; ?>"><br><br>
+    <input class="form-control border border-info" type="text" id="lname" name="lname" value="<?php echo isset($_POST['lname']) ? htmlspecialchars($_POST['lname']) : ''; ?>"><br><br>
     <p class="error error_lname"><?php echo $lname_error; ?></p>
 
     <label class="form-label" for="email">Email:</label>
-    <input class="form-control" type="email" id="email" name="email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>"><br><br>
+    <input class="form-control border border-info" type="email" id="email" name="email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>"><br><br>
     <p class="error error_email"><?php echo $email_error; ?></p>
     <div>
     <label>Select your gender:</label>
@@ -128,7 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
     </div>
     <div>
     <label for="country">Choose a Country:</label>
-    <select name="country" id="country">
+    <select class="form-selec border border-info" name="country" id="country">
         <option value="">Select a country</option>
         <option value="Italy" <?php echo (isset($_POST['country']) && $_POST['country'] == 'Italy') ? 'selected' : ''; ?>>Italy</option>
         <option value="Belgium" <?php echo (isset($_POST['country']) && $_POST['country'] == 'Belgium') ? 'selected' : ''; ?>>Belgium</option>
@@ -139,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
     </div>
     <div>
     <label for="subject">Choose a subject:</label>
-    <select name="subject" id="subject">
+    <select class="form-select border border-info" name="subject" id="subject">
         <option value="">Select a subject</option>
         <option value="learning" <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'learning') ? 'selected' : ''; ?>>Learning</option>
         <option value="gaming" <?php echo (isset($_POST['subject']) && $_POST['subject'] == 'gaming') ? 'selected' : ''; ?>>Gaming</option>
@@ -147,12 +148,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
     </select><br><br>
     <p class="error error_subject"><?php echo $subject_error; ?></p>
     </div>
-
+<div class="mb-3">
     <label class="form-label" for="message">Enter a Message:</label>
-    <textarea id="message" name="message"><?php echo isset($_POST['message']) ? htmlspecialchars($_POST['message']) : ''; ?></textarea><br><br>
+    <textarea class="form-control border border-info" id="message" name="message"><?php echo isset($_POST['message']) ? htmlspecialchars($_POST['message']) : ''; ?></textarea><br><br>
     <p class="error error_message"><?php echo $message_error; ?></p>
-
-    <input type="submit" value="Submit">
+</div>
+    <input class="btn btn-success mb-n1" type="submit" value="Submit">
 </form>
 </main>
 </body>
